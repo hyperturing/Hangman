f3961c44c17dee12a1ca68e9bebf169018579722
Ash Peterson <ashnight07@gmail.com>
2020-01-16 12:20:26 -0600

    Input validation added

--- board.rb
+++ board.rb
@@ -5,13 +5,13 @@ class Board
     words = open('5desk.txt', 'r').read.split("\r")
     @secret_word = words.sample.gsub!(/[^0-9A-Za-z]/, '').downcase.split('')
     @letters_left = @secret_word.dup
+    @correct_letters = []
     @guesses = []
     @incorrect_guesses = 0
   end
 
   def display_board
-    puts "Secret word: #{@secret_word} \n\n"
-    puts "letters left: #{letters_left}\n\n"
+    puts "Secret word: #{display_word} \n\n"
     puts "Guesses: #{guesses}\n\n"
   end
 
@@ -26,10 +26,20 @@ class Board
   def evaluate_guess(guess)
     @guesses.push(guess)
     if @letters_left.include?(@guesses.last)
-      @letters_left.delete_at(@letters_left.index(@guesses.last))
+      @letters_left.delete(@guesses.last)
+      @correct_letters.push(@guesses.last)
     else
       puts "Letter #{@guesses.last} not found in word\n\n"
       @incorrect_guesses += 1
     end
+  end
+
+  def display_word
+    @secret_word.map do |letter|
+      @guesses.include?(letter) ? " #{letter} " : ' _ '
+    end
+  end
+  def display_hangman
+
   end
 end
--- player.rb
+++ player.rb
@@ -2,12 +2,22 @@ class Player
   attr_reader :guess, :name
 
   def initialize(name)
-    @guess = []
+    @guess = ''
     @name = name
   end
 
   def make_guess
     puts 'Enter a guess:'
-    @guess = gets.chomp.downcase
+    @guess = gets.chomp
+    until validated?(@guess)
+      puts 'Invalid guess'
+      puts 'Enter a guess:'
+      @guess = gets.chomp
+    end
+    @guess
+  end
+
+  def validated?(guess)
+    guess.match?(/[a-zA-Z]/)
   end
 end
